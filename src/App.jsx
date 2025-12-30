174b1dade519904421f8d6017978d7add495923c
LuisArambula <luis.arambula@outlook.es>
2025-12-29 22:10:05 -0800

    el mensaje de confirmacion..

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="page__content">
+      <header>
+        <div className="header">
+          <div className="header__top">
+            <img src="images/Vector.png" alt="around" />
+          </div>
+
+          <div className="header__bottom">
+            <div>
+              <img
+                className="header__bottom-imagen"
+                src="images/image.jpg"
+                alt="foto de perfil"
+              />
+            </div>
+
+            <div>
+              <div className="header__button-items">
+                <h1 className="header__bottom-name">Jacques Cousteau</h1>
+                <button className="header__bottom-editprofile"></button>
+              </div>
+              <p className="header__bottom-description">Explorador</p>
+            </div>
+
+            <button className="header__bottom-addbutton"></button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <section className="cards">
+          <div className="cards__container">
+            {/* Card 1 */}
+            <div className="card">
+              <button className="card__delete-button">
+                <img
+                  src="images/Trash.svg"
+                  className="card__delete-imagen"
+                  alt="trash icon"
+                />
+              </button>
+
+              <img
+                src="images/valledeRosemite2.jpg"
+                alt="Valle de Yosemite"
+                className="card__imagen"
+              />
+
+              <div className="card__container">
+                <h2 className="card__tittle">Valle de Yosemite</h2>
+                <button className="card__like-button">
+                  <img
+                    src="images/likebotton.svg"
+                    alt="corazon"
+                    className="like-icon"
+                  />
+                </button>
+              </div>
+            </div>
+
+            {/* Aquí luego se mapearán más cards */}
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <div className="footer">
+          <p className="footer__text">© 2024 Around The U.S.</p>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
